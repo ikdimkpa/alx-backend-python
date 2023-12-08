5d1bf26471e18fbbfc93f1252e9bfe3d2bf62fed
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2023-12-08 09:52:23 +0000

    task 0 and readme file

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Type-annotated function add that takes a float a and a float b
+as arguments and returns their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Takes in two arguments of type float and returns the
+    sum of both arguments as a float
+    """
+
+    return a + b
