176df67a686f378907695565234e84003e75d2b1
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2023-12-12 00:07:01 +0000

    debugged 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,9 +1,26 @@
-#!/usr/bin/env python3
-import asyncio
+#!//bin/env python3
+"""Double co-routines with tasks"""
+from typing import List
 
-task_wait_n = __import__('4-tasks').task_wait_n
+task_wait_random = __import__('3-tasks').task_wait_random
 
-n = 5
-max_delay = 6
 
-print(asyncio.run(task_wait_n(n, max_delay)))
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """run wait random n times"""
+    delay_list: List[float] = []
+    for _ in range(n):
+        task = task_wait_random(max_delay)
+        delay: float = await task
+        # print(f"delay: {delay}")
+        delay_list.append(delay)
+    length = len(delay_list)
+    for i in range(length):
+        swapped = False
+        for j in range(0, n-i-1):
+            if delay_list[j] > delay_list[j+1]:
+                delay_list[j], delay_list[j+1] = delay_list[j+1], delay_list[j]
+                swapped = True
+        if not swapped:
+            break
+
+    return delay_list
